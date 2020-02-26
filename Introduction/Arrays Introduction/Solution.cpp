d19a20e8bf5a0e640782aeacc869c1205bb987e0
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-26 16:14:19 +0530

    Introduction to C++!

---
+++ Introduction/Arrays Introduction/Solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int n,i;
+cin>>n;
+
+int*a=new int[n];
+int*b=new int[n];
+
+for(i=0;i<n;i++)
+    {
+    cin>>a[i];
+    }
+for (i=0;i<n;i++)
+    {
+    b[i]=a[n-1-i];
+    }
+for (i=0;i<n;i++)
+    {
+    cout<<b[i]<<" ";
+    }
+return 0;
+}
