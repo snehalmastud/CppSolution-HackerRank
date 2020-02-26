6a982740059658a7e7b69120fc2c019b35e722ec
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-26 16:22:53 +0530

    Classes!

---
+++ Classes/Exceptional Server/Solution.cpp
@@ -0,0 +1,12 @@
+
+
+		/* Enter your code here. */
+try {
+          cout << Server::compute(A, B) << "\n";
+        } catch (const std::bad_alloc& e) {
+            cout << "Not enough memory\n";
+        } catch (const std::exception& e) {
+            cout << "Exception: " << e.what() << "\n";
+        } catch (...) {
+            cout << "Other Exception\n";
+        }
