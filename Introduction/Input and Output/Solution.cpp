820d3971e424c46dc619896997945da5f3ffed47
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-26 16:09:31 +0530

    Introduction to C++!

---
+++ Introduction/Input and Output/Solution.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int a,b,c;
+void input()
+{
+cin>>a>>b>>c;
+}
+
+int sum()
+{
+cout<<a+b+c;
+return 0;
+}
+
+int main()
+{
+input();
+sum();
+return 0;
+}
