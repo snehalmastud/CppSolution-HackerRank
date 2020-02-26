c314cf0114519c1c8699193c1d127c4065523474
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-26 16:13:30 +0530

    Introduction to C++!

---
+++ Introduction/Pointer/Solution.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int a,b;
+int main()
+{
+cin>>a;
+cin>>b;
+int*pa=&a;
+int*pb=&b;
+
+cout<<*pa+*pb<<endl;
+
+if(a>b) {cout<<*pa-*pb<<endl;}
+else cout<<*pb-*pa<<endl;
+}
